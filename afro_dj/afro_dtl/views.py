0148ebe4aa8bad0023f9e1688c2fefc1575d0247
olive <bon_olives@yahoo.com>
2023-04-12 10:45:33 +0300

    my initial commit

--- afro_dj/afro_dtl/views.py
+++ afro_dj/afro_dtl/views.py
@@ -3,4 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     pr_title = 'Afro-Django'
-    return render(request, 'index.html', {'pr_title': pr_title})
+    username = 'Olive'
+    gender = 'Female'
+    return render(
+        request, 
+        'index.html', 
+        {'pr_title': pr_title, 'username':username, 'gender':gender}
+        )
+
+def register(request):
+    return render(request, 'register.html')
